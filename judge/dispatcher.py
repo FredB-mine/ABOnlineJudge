e1ce32f4119ad52e5c159e435ce630637ef0e0e4
FredB-mine <fredbao1126@gmail.com>
2023-07-24 16:06:09 +0800

    bugfixes

--- judge/dispatcher.py
+++ judge/dispatcher.py
@@ -1,15 +1,17 @@
 import hashlib
 import json
 import logging
+from typing import Union
 from urllib.parse import urljoin
 
 import requests
-from django.db import transaction, IntegrityError
+from django.db import IntegrityError, transaction
 from django.db.models import F
 
 from account.models import User
 from conf.models import JudgeServer
-from contest.models import ContestRuleType, ACMContestRank, OIContestRank, ContestStatus
+from contest.models import (ACMContestRank, ContestRuleType, ContestStatus,
+                            OIContestRank)
 from options.options import SysOptions
 from problem.models import Problem, ProblemRuleType
 from problem.utils import parse_problem_template
@@ -35,7 +37,7 @@ class ChooseJudgeServer:
     def __init__(self):
         self.server = None
 
-    def __enter__(self) -> [JudgeServer, None]:
+    def __enter__(self) -> Union[JudgeServer, None]:
         with transaction.atomic():
             servers = JudgeServer.objects.select_for_update().filter(is_disabled=False).order_by("task_number")
             servers = [s for s in servers if s.status == "normal"]
