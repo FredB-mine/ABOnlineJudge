941f4b43971fe1fc76f7eaaeb73d382e89c70fbd
FredB-mine <fredbao1126@gmail.com>
2023-07-25 09:19:24 +0800

    bugfixes

--- deploy/health_check.py
+++ deploy/health_check.py
@@ -1,9 +1,7 @@
 import xmlrpc.client
-from time import sleep
 
 if __name__ == "__main__":
     try:
-        sleep(10)
         with xmlrpc.client.ServerProxy("http://localhost:9005/RPC2") as server:
             info = server.supervisor.getAllProcessInfo()
             error_states = list(filter(lambda x: x["state"] != 20, info))
